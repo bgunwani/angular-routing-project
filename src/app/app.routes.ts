e895050dce5adf045b629b4612e94b116d5ebc17
bgunwani <bhawna.gunwani@gmail.com>
2025-02-04 10:38:08 +0530

    Third Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { UsersComponent } from './users/users.component';
 
 // Configuring Routes Here
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'users', component: UsersComponent }
 ];
